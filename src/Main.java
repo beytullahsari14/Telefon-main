ed78578b449d1f57ab705615ea5b120fe841c0fe
beytullahsari14 <beytullahsari14@yandex.com>
2021-11-09 16:27:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import jdk.internal.foreign.abi.Architecture;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String phoneName="Xiaomi Redmi Note 8 Pro 128 GB";
+        double phonePrice=3282.24;
+        double phonestar=4.8;
+        boolean phoneIsDiscound=true;
+        int phonediscound=7;
+        
+        
+	}
+
+}
